f648989e55bed1c62a69e9ccf3dc3676f2b322b0
Алексей Чупров <alexey.chuprov@gmail.com>
2019-11-21 17:43:15 +0300

    update 2019.11.21 17:43:12

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -58,7 +58,7 @@ const requiredFiles = [
 "/default-icon-192x192.png",
 "/default-icon-512x512.png",
 "/favicon.ico",
-"/Department/index.html",
+"/index.html",
 "/sample-data/weather.json",
 "/ServiceWorkerRegister.js",
 "/manifest.json"
